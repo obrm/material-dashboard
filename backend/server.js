093759f20e84dc03936955309f37f2735ffd7db1
Ori Baram <obrm770@gmail.com>
2021-05-31 14:28:08 +0300

    initial server setup

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
+
+const app = express()
+
+dotenv.config()
+
+connectDB()
+
+app.get('/', (req, res) => {
+  res.send('API is running')
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
