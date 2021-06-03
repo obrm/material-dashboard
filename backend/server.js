0b99eead7bc56533ce9c32a1388b5f55346c9e2b
Ori Baram <obrm770@gmail.com>
2021-06-03 12:59:33 +0300

    prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -20,14 +20,14 @@ app.use('/api/users', userRoutes)
 const __dirname = path.resolve()
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.use(express.static(path.join(__dirname, 'frontend/build')))
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {
-    res.send('API is running')
+    res.send('API is running...')
   })
 }
 
