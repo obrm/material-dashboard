b349965b2a3c355f01353d755ab87f37304e38db
Ori Baram <obrm770@gmail.com>
2021-06-03 11:50:22 +0300

    fixed error handler for registration errors

--- frontend/src/redux/helper/errorHandler.js
+++ frontend/src/redux/helper/errorHandler.js
@@ -1,5 +1,16 @@
+const parseErrorResponse = (error) => {
+  let message = ''
+  // eslint-disable-next-line
+  for (const obj of error) {
+    message += `${obj.msg}. `
+  }
+  return message
+}
+
 export const errorHandler = (error) => {
-  return error.response && error.response.data.message
+  return error.response.data.errors
+    ? parseErrorResponse(error.response.data.errors)
+    : error.response && error.response.data.message
     ? error.response.data.message
     : error.message
 }
