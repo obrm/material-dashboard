a89d33f82fd6408c65598a94505e35318f42d94f
Ori Baram <obrm770@gmail.com>
2021-06-03 12:20:42 +0300

    PREPARE TO DEPLOY

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
@@ -14,11 +15,21 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('API is running')
-})
-
 app.use('/api/users', userRoutes)
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running')
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler)
